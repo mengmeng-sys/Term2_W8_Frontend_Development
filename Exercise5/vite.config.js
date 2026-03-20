b122707076aaad056be971690333a6cb6f0da642
mengmeng-sys <na634997@gmail.com>
2026-03-20 10:15:53 +0700

    Finish ex5

--- Exercise5/vite.config.js
+++ Exercise5/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
+  resolve:{
+    alias:{
+      '@ex1':path.resolve(__dirname,'../Exercise1/src'),
+      '@ex3':path.resolve(__dirname,'../Exercise3/src'),
+    }
+    
+  }
 })
